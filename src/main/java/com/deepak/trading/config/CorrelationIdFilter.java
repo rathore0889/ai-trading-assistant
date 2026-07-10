6483229b3a8034bffc26b48fe7f3eb36f127f63c
Deepak Rathore <rathore0889@gmail.com>
2026-07-10 17:31:43 +0530

    added coreleationId

---
+++ src/main/java/com/deepak/trading/config/CorrelationIdFilter.java
@@ -0,0 +1,46 @@
+package com.deepak.trading.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class CorrelationIdFilter extends OncePerRequestFilter {
+
+    public static final String CORRELATION_ID = "correlationId";
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String requestId = UUID.randomUUID()
+                .toString()
+                .substring(0, 8);
+
+        MDC.put(CORRELATION_ID, requestId);
+
+        response.setHeader("X-Correlation-Id", requestId);
+
+        try {
+
+            filterChain.doFilter(request, response);
+
+        } finally {
+
+            MDC.clear();
+
+        }
+
+    }
+
+}
