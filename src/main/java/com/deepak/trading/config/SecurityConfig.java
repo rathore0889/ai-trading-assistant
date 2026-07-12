c4438424d4522f3d5cb4dc76c8e59d9d94d4d04c
Deepak Rathore <rathore0889@gmail.com>
2026-07-12 21:34:47 +0530

    flyway implenet

--- src/main/java/com/deepak/trading/config/SecurityConfig.java
+++ src/main/java/com/deepak/trading/config/SecurityConfig.java
@@ -72,7 +72,8 @@ public class SecurityConfig {
                                 "/api/auth/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/actuator/**"
                         ).permitAll()
 
                         .anyRequest().authenticated());
